ca1142f8809912cde1d5b62cc801a565dac560ce
Russell Gordon <rgordon@lcs.on.ca>
2024-06-07 16:17:07 -0400

    Now shows hex value for string.

--- HexColorExample/ContentView.swift
+++ HexColorExample/ContentView.swift
@@ -5,11 +5,16 @@
 //  Created by Russell Gordon on 2024-06-07.
 //
 
+import NVMColor
 import SwiftUI
 
 struct ContentView: View {
     
-    @State private var currentColor: Color = Color.yellow
+    @State private var currentColor: Color = Color.white
+    
+    var hexRepresentation: String {
+        return String(currentColor.hex?.isHex()?.dropLast(2) ?? "FFFFFF")
+    }
     
     var body: some View {
         
@@ -21,10 +26,11 @@ struct ContentView: View {
             
             // Foreground layer
             VStack {
-                Image(systemName: "globe")
-                    .imageScale(.large)
-                    .foregroundStyle(.tint)
-                Text("Hello, world!")
+                
+                ColorPicker("Background", selection: $currentColor)
+                
+                Text(hexRepresentation)
+                
             }
             .padding()
 
