1452af98df0e0cb160699c65ba973eb093c983c6
Russell Gordon <rgordon@lcs.on.ca>
2024-06-07 16:05:26 -0400

    Initial Commit

---
+++ HexColorExample/HexColorExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  HexColorExampleApp.swift
+//  HexColorExample
+//
+//  Created by Russell Gordon on 2024-06-07.
+//
+
+import SwiftUI
+
+@main
+struct HexColorExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
