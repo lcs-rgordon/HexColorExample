b141dd695fa9324c79947bb0715ae8600f519213
Russell Gordon <rgordon@lcs.on.ca>
2024-06-07 16:07:35 -0400

    View made to show background color and added package dependency.

--- HexColorExample/ContentView.swift
+++ HexColorExample/ContentView.swift
@@ -8,14 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var currentColor: Color = Color.yellow
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        ZStack {
+            
+            // Background layer
+            currentColor
+                .ignoresSafeArea()
+            
+            // Foreground layer
+            VStack {
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundStyle(.tint)
+                Text("Hello, world!")
+            }
+            .padding()
+
         }
-        .padding()
     }
 }
 
